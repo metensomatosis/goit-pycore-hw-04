d91e4787ea040544b3ea84818913dc3eedaced2e
metensomatosis <vkrasnovskyy@gmail.com>
2026-03-15 22:38:19 +0200

    Update Module6_T2.py

--- Module6_T2.py
+++ Module6_T2.py
@@ -1,31 +1,33 @@
-def total_salary(path: str) -> tuple:
-    total = 0
-    count = 0
+def total_salary(path: str) -> tuple[int, int]:
+    total: int = 0
+    count: int = 0
 
     try:
         with open(path, "r", encoding="utf-8") as file:
             for line in file:
                 line = line.strip()
+
                 if not line:
-                    continue  # пропускаємо порожні рядки
+                    continue
 
                 name, salary = line.split(",")
                 total += int(salary)
                 count += 1
 
         if count == 0:
-            return (0, 0)
+            return 0, 0
 
-        average = total / count
-        return (total, average)
+        average: int = total // count
+        return total, average
 
     except FileNotFoundError:
         print("Файл не знайдено.")
-        return (0, 0)
-
+        return 0, 0
     except ValueError:
         print("Помилка в форматі даних у файлі.")
-        return (0, 0)
+        return 0, 0
 
-total, average = total_salary("salary.txt")
-print(f"Загальна сума заробітної плати: {total}, \nСередня заробітна плата: {average}")
+
+if __name__ == "__main__":
+    total, average = total_salary("salary.txt")
+    print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
